4db98974ceb183c4442b13850d66ec6189285044
JanghoPark <pjhsk113@naver.com>
2023-07-22 16:45:11 +0900

    Initial commit

---
+++ src/test/java/io/study/demo/multimoduleexample/MultiModuleExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package io.study.demo.multimoduleexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiModuleExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
