7d76a7f9f33bd6c7fb47b09d2ed12d6d5d7c1f58
JanghoPark <pjhsk113@naver.com>
2023-07-30 14:18:15 +0900

    feat: 도메인 모듈 - data 객체 추가

---
+++ domain/src/main/java/user/User.java
@@ -0,0 +1,11 @@
+package user;
+
+public class User {
+    private Long id;
+    private String name;
+
+    public User(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
